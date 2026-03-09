76e0f29e5d5a64e9b966572003fcba221e8c2929
JuliaMaxx <jjuliamaxxx@gmail.com>
2026-03-09 19:47:07 +0100

    chore: create POC of file uploading

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,18 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File
 
 app = FastAPI()
 
 @app.get("/")
 def root():
     return {"message": "Genomic API is running"}
+
+
+@app.post("/upload-dna/")
+async def upload_dna(file: UploadFile = File(...)):
+
+    contents = await file.read()
+
+    return {
+        "filename": file.filename,
+        "size": len(contents)
+    }
